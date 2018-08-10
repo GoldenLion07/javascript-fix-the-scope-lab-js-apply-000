77833eb7c620aab63e31b3599d080c4d66734001
gmat191@gmail.com <gmat191@gmail.com>
2018-08-10 22:43:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'cat'
+var animal = 'dog'
 
 function myAnimal() {
   return animal
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return myAnimal
-  return animal
+  
 }
 
 function add2(n) {
